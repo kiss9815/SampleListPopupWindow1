34c2a4f4f68dcdab54ac7a4ae05c23f8152f5a2d
kiss9815 <kiss9815@gmail.com>
2016-01-28 19:44:20 +0900

    edit 텍스트에 값을 입력하면 리스트 popup 이 뜨게 함

---
+++ app/src/main/java/com/example/tacademy/samplelistpopupwindow1/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.tacademy.samplelistpopupwindow1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListPopupWindow;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListPopupWindow popupWindow;
+    EditText keywordView;
+    ArrayAdapter<String> mAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        popupWindow = new ListPopupWindow(this);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        popupWindow.setAdapter(mAdapter);
+
+        keywordView = (EditText)findViewById(R.id.edit_keyword);
+        popupWindow.setAnchorView(keywordView);
+
+        popupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                keywordView.setText(mAdapter.getItem(position));
+                popupWindow.dismiss();
+            }
+        });
+
+        keywordView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = s.toString();
+                if(!TextUtils.isEmpty(text)&& text.length()>2){// 텍스트가 길이가 2이고 있으면
+                    mAdapter.clear();
+                    for(int i =0; i< 3; i++){
+                        mAdapter.add(text +":"+i);
+                    }
+                    popupWindow.show();
+                }else {
+                    popupWindow.dismiss();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
